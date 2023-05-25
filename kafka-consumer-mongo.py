b59c7e68a851e0a57e4bd863985dc5c7cb979c38
FerminDRA <zs19004879@estudiantes.uv.mx>
2023-05-25 16:20:08 -0600

    add summary

--- kafka-consumer-mongo.py
+++ kafka-consumer-mongo.py
@@ -9,6 +9,7 @@ from pymongo.server_api import ServerApi
 import json
 
 uri = "mongodb+srv://shin16:PxN85ONiYd8GtGM8@nosql.tndoycz.mongodb.net/?retryWrites=true&w=majority"
+#uri="mongodb://127.0.0.1:27017/nosql"
 
 # Create a new client and connect to the server
 #client = MongoClient(uri, server_api=ServerApi('1'))
@@ -32,7 +33,31 @@ try:
 except:
     print("Could not connect to MongoDB")
 
-consumer = KafkaConsumer('test',bootstrap_servers=['my-kafka-0.my-kafka-headless.fermindra.svc.cluster.local:9092'])
+consumer = KafkaConsumer('test',bootstrap_servers=[
+    'my-kafka-0.my-kafka-headless.fermindra.svc.cluster.local:9092'
+    #'localhost:9092'
+    ])
+
+#count
+try:
+    agg_result=db.nosql_info.aggregate(
+    [
+        {
+        "$group":
+        {
+            "_id":"$name",
+            "n":{"$sum":1}
+        }}
+    ])
+    db.nosql_summary.delete_many({})
+    for i in agg_result:
+        print(i)
+        summary_id= db.nosql_summary.insert_one(i)
+        print("Summary inserted  with records  ids",summary_id)
+except Exception as e:
+    print(f'Group by caught {type(e)}:')
+    print(e)
+
 # Parse received data from Kafka
 for msg in consumer:
     record = json.loads(msg.value)
@@ -47,3 +72,22 @@ for msg in consumer:
        print("Data inserted with record ids", meme_id)
     except:
        print("Could not insert into MongoDB")
+
+    try:
+        agg_result=db.nosql_info.aggregate(
+        [
+            {
+            "$group":
+            {
+                "_id":"$name",
+                "n":{"$sum":1}
+            }}
+        ])
+        db.nosql_summary.delete_many({})
+        for i in agg_result:
+            print(i)
+            summary_id= db.nosql_summary.insert_one(i)
+            print("Summary inserted  with records  ids",summary_id)
+    except Exception as e:
+        print(f'Group by caught {type(e)}:')
+        print(e)
